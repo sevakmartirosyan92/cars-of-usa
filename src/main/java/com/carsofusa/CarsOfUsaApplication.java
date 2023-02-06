7eec1914942131eb5a625ff33a16e27ff3bf52c0
Sevak <sevakmarti@gmail.com>
2023-02-06 16:13:43 +0400

    Initial commit

---
+++ src/main/java/com/carsofusa/CarsOfUsaApplication.java
@@ -0,0 +1,13 @@
+package com.carsofusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsOfUsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsOfUsaApplication.class, args);
+    }
+
+}
